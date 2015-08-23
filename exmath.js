47c0d753a294d0354ccd1f091c21769c8871df6a
koteitan <ukaibutton@mac.com>
2015-08-23 19:17:57 +0900

    interface of XorShift is changed.(still has bug in that randomness is lost)

--- exmath.js
+++ exmath.js
@@ -11,40 +11,35 @@ var ln = function(x){
 var chisqr_1_0p05 = function(){
   -3.841
 }
-
 //xorshift random object
-var XorShift={
-  x: 123456789,
-  y: 362436069,
-  z: 521288629,
-  w: 88675123
+var XorShift=function(s){
+  if(s==undefined) s=(new Date()).getTime();
+  this.x=123456789;
+  this.y=362436069;
+  this.z=521288629;
+  this.seed=s;
+  this.w=this.seed;
+  this.getNext();
 };
 // random float value 0.0<=x<+1.0
-XorShift.getNext = function() {
-  var t = XorShift.x ^ (XorShift.x << 11);
-  XorShift.x = XorShift.y;
-  XorShift.y = XorShift.z;
-  XorShift.z = XorShift.w;
-  XorShift.w = (XorShift.w^(XorShift.w>>>19))^(t^(t>>>8));
-  return XorShift.w/(1<<31)/2+1/2;
-}
-// reset the seed
-XorShift.setSeed = function(s) {
-  XorShift.x = 123456789;
-  XorShift.y = 362436069;
-  XorShift.z = 521288629;
-  XorShift.w = s;
-}
+XorShift.prototype.getNext = function() {
+  var t = this.x ^ (this.x << 11);
+  this.x = this.y;
+  this.y = this.z;
+  this.z = this.w;
+  this.w = (this.w^(this.w>>>19))^(t^(t>>>8));
+  return this.w/(1<<31)/2+1/2;
+};
 //test randomness
 XorShift.test = function(seed){
   var str="";
-  XorShift.setSeed(seed);
+  var xs=new this(seed);
   var sumx=0;
   var maxx=-Infinity;
   var minx=+Infinity;
   var N=100000;
   for(var i=0;i<N;i++){
-    var x=XorShift.getNext();
+    var x=this.getNext();
     sumx+=x;
     if(x>maxx)maxx=x;
     if(x<minx)minx=x;
