17ef553e1df71521aeeac4aa442d22caa8fc8876
koteitan <ukaibutton@mac.com>
2015-08-23 23:05:46 +0900

    the comment of change log and known bugs are added to web page.

--- 4dgolf_version.js
+++ 4dgolf_version.js
@@ -1,1 +1,6 @@
-document.getElementById("versiondisplay").innerHTML = "0.1";
+document.getElementById("versiondisplay").innerHTML = "0.2";
+document.getElementById("changelog").innerHTML = 
+"<ul>"+
+"<li>2015.08.23 23:00 マウスでカメラ操作を実装中。</li>"+
+"</ul>";
+document.getElementById("knownbugs").innerHTML = "カメラの動きがまだおかしい。";
