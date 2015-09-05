a2b95da8b902642bd8e77a9e82f683e1e7127d33
koteitan <ukaibutton@mac.com>
2015-09-06 03:51:28 +0900

    CanvasRenderingContext2D.measureTextBitmap() is added.

--- excanvas.js
+++ excanvas.js
@@ -19,8 +19,9 @@ CanvasRenderingContext2D.prototype.setBitmapFont = function(url, poslist, sizeli
   }
 };
 /* ctx.fillTextBitmap(text,x,y) draw text with the bitmap font set by the setBitmapFont().
-    same as fillText(text,x,y) 
-    x,y = 
+    same as CanvasRenderingContext2D.fillText(text,x,y),
+      text = text to be written
+      x,y = position in ctx to put the text.
     */
 CanvasRenderingContext2D.prototype.fillTextBitmap = function(text, x, y){
   var px=x;
@@ -45,4 +46,48 @@ CanvasRenderingContext2D.prototype.fillTextBitmap = function(text, x, y){
     }
   }
 };
+/* ctx.measureTextBitmap(text) returns size of text in the bitmap font which set with setBitmapFont().
+  almost same as width in px = CanvasRenderingContext2D.measureText(text),
+    text = text to be written
+    ctx.measureTextBitmap(text) = [wx,wy]:
+      wx = width in px
+      wy = height in px
+    
+  */
+CanvasRenderingContext2D.prototype.measureTextBitmap = function(text){
+  var px=0;
+  var py=0;
+  var maxpx=0;
+  var poslist=this.bmfont.poslist;
+  var sizelist=this.bmfont.sizelist;
+  var letterlist=this.bmfont.letterlist;
+  for(i=0;i<text.length;i++){
+    if(text[i]=='\n'){
+      py=py+this.bmfont.maxheight;
+      px=0;
+    }else{
+      var j=letterlist.indexOf(text[i]); // from "A"
+      px+=sizelist[j][0];
+      maxpx=(px>maxpx)?px:maxpx;
+    }
+  }
+  return [px,py+this.bmfont.maxheight];
+};
+CanvasRenderingContext2D.prototype.guideText = function(text,px,py){
+  var tz=this.measureTextBitmap(text);
+  var linex=40;
+  var liney=20;
+  //draw line
+  this.beginPath();
+  this.moveTo(px   ,py   );
+  this.lineTo(px+linex,py+liney);
+  this.stroke();
+  this.beginPath();
+  this.moveTo(px+linex      ,py+liney);
+  this.lineTo(px+linex+tz[0],py+liney);
+  this.stroke();
+  this.beginPath();
+  //draw text
+  this.fillTextBitmap(text,px+linex, py+liney-tz[1]);
+}
 
